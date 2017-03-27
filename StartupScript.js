e2e7897555e778b018a0f410a9468b75044b4776
lmsitetest2017 <lmsitetest2017@gmail.com>
2017-03-27 12:11:35 +0300

    Add files via upload

---
+++ StartupScript.js
@@ -0,0 +1,46 @@
+﻿console.log("start");
+var test3 = 12345;
+//function start() {
+//    console.log("start");
+//    // Create script object
+//    var script = document.createElement('script');
+//    var fileName = "ExternalCatalog.js";
+//    // add cache bursting - assuming the script has stored the version in a variable called EXTERNAL_CATALOG_VERSION
+//    if (!EXTERNAL_CATALOG_VERSION) {
+//        var EXTERNAL_CATALOG_VERSION = "19.6.0";
+//    }
+//    fileName += "?version=" + EXTERNAL_CATALOG_VERSION;
+//    // Build script URL
+//    //var startURL = document.referrer.replace("https://", "").split("/")[0];
+//    var startURL = document.location.href.replace("https://", "").split("/")[0];
+//    console.log("url: " + startURL);
+//    script.src = "https://" + startURL + "/" + fileName;
+//    script.onload = loaded;
+//    document.head.appendChild(script);
+//}
+
+//function loaded() {
+//    //Connection to the catalog
+//    console.log("loaded");
+
+//    catalog.connect(
+//      {
+//          "scope": window,
+//          fn: subscribeToEvents
+//      }
+//    );
+//}
+
+//function subscribeToEvents() {
+//    //Event subscription
+//    console.log("connected");
+
+//    catalog.subscribe(
+//      "commandpage.notifyResponse",
+//      {
+//          fn: function (data, cbData) { console.log(data); console.log(cbData); },
+//          scope: window,
+//          params: "hello"
+//      }
+//    );
+//}
